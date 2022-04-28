1a9e91925d3231c5210bbe3e0f3c9045777de592
benjalonso <alonsobenjaminlb@gmail.com>
2022-04-28 19:52:09 +0000

    casi listo

--- src/js/component/songList.jsx
+++ src/js/component/songList.jsx
@@ -1,13 +1,14 @@
-import { faPersonDigging } from "@fortawesome/free-solid-svg-icons";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 const SongList = ({ songs }) => {
-	// const [selectedSong, setSelectedSong] = useState(null);
+	const [selectedSong, setSelectedSong] = useState(null);
 	let refSong = useRef(null);
+
 	// const selectSong = () => {};
-	const setSong = (link) => {
+	const setSong = (link, index) => {
 		refSong.src = `https://assets.breatheco.de/apis/sound/${link}`;
 		refSong.play();
+		setSelectedSong(index);
 	};
 	const playSong = () => {
 		refSong.play();
@@ -16,7 +17,20 @@ const SongList = ({ songs }) => {
 		refSong.pause();
 	};
 	const nextSong = (index) => {
-		refSong.src[index] + 1;
+		if (index + 1 >= songs.length) {
+			index = 0;
+		} else {
+			index = index + 1;
+		}
+		setSong(songs[index].url, index);
+	};
+	const previousSong = (index) => {
+		if (index - 1 <= 0) {
+			index = songs.length - 1;
+		} else {
+			index = index - 1;
+		}
+		setSong(songs[index].url, index);
 	};
 	return (
 		<>
@@ -25,7 +39,7 @@ const SongList = ({ songs }) => {
 					songs.map(({ name, url }, index) => {
 						return (
 							<li
-								onClick={() => setSong(url)}
+								onClick={() => setSong(url, index)}
 								key={index}
 								className="listOfSongs list-group-item list-group-item-action">
 								{name}
@@ -43,14 +57,16 @@ const SongList = ({ songs }) => {
 
 			<i className="fa-solid fa-circle-play playTop"></i>
 			<div className=" barraControl container-fluid row fixed-bottom ">
-				<i className="iconos col-3 fas fa-caret-square-left"></i>
+				<i
+					className="iconos col-3 fas fa-caret-square-left"
+					onClick={() => previousSong(selectedSong)}></i>
 				<i className="iconos col-3 fas fa-play " onClick={playSong}></i>
 				<i
 					className="iconos col-3 fas fa-pause "
 					onClick={pauseSong}></i>
 				<i
 					className="iconos col-3 fas fa-caret-square-right"
-					onClick={nextSong}></i>
+					onClick={() => nextSong(selectedSong)}></i>
 			</div>
 			<audio ref={(r) => (refSong = r)} className="barraControl" />
 		</>
